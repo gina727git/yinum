71e23a71325ae5cc4e10dbd330342dc8c42c15a4
gina727git <168347481+gina727git@users.noreply.github.com>
2024-05-01 17:04:47 +0800

    remove loadFooter

--- scripts/yiNumLayout.js
+++ scripts/yiNumLayout.js
@@ -3,19 +3,6 @@ class LayoutService {
     constructor() {
     }
 
-    loadFooter(){
-      window.addEventListener('DOMContentLoaded', function() {
-        const footerContainer = document.getElementsByTagName('footer');
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', './common/footer.html', true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                footerContainer.innerHTML = xhr.responseText;
-            }
-        };
-        xhr.send();
-    });
-    }
 
     cleanTable(divId) {
         const resultDiv = document.getElementById(divId)
